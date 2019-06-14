913384bcfed2250538402c3efcb61a365cea8e0d
Jackson Yang <jacksony@gmail.com>
2019-06-14 16:47:08 +0800

    debug

--- server.js
+++ server.js
@@ -19,8 +19,6 @@ app.post('/file-upload',upload.single('file'), function (req, res) {
         res.json(req.file.path) 
         return;
   }); 
-
-
 app.listen(3000, function () {
     console.log('Listening on port 3000!');
   });
